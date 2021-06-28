fd4ac9af33aab394dd170fd97bb55373e78b1ef2
Ananya Singhal <48484581+reachananya@users.noreply.github.com>
2021-06-28 16:34:36 +0530

    Create config.js

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+//check env
+
+var env = process.env.NODE_ENV || 'development';
+
+//fetch env. config
+var config = require('./config.json');
+var envConfig = config[env];
+
+Object.keys(envConfig).forEach(key => process.env[key] = envConfig[key]); 
